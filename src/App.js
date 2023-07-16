3f206feff0b43f4439172e36e0a94586ec5ad2f0
Anus Ali Khan <anusalikhan1405@gmail.com>
2023-07-16 23:49:46 +0500

    navbar edit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from "./pages/home"
+import CreatePost from "./pages/CreatePost"
+import Login from "./pages/Login"
+
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <nav>
+        <Link to='/' >Home </Link>
+        <Link to='/createpost' >Create Post </Link>
+        <Link to='/login' > Login</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/createpost' element={<CreatePost />} />
+        <Route path='/login' element={<Login />} />
 
-    </div>
+      </Routes>
+    </Router>
   );
 }
 
