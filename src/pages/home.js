a3b8f08e16d9054091957ebdffc08abab36f4e89
Anus Ali Khan <anusalikhan1405@gmail.com>
2023-10-28 21:58:50 +0500

    Change Title

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,42 +1,54 @@
-import { getDocs, collection, deleteDoc, doc } from 'firebase/firestore';
+import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import { db, auth } from '../firebase-config';
-import React from 'react';
+import { db, auth } from "../firebase-config";
+import React from "react";
 
 function Home({ isAuth }) {
-    const [postLists, setPostList] = useState([]);
-    const postsCollectionRef = collection(db, "posts")
+  const [postLists, setPostList] = useState([]);
+  const postsCollectionRef = collection(db, "posts");
 
-    const deletePost = async (id) => {
-        const postDoc = doc(db, "posts", id)
-        await deleteDoc(postDoc)
-    }
+  const deletePost = async (id) => {
+    const postDoc = doc(db, "posts", id);
+    await deleteDoc(postDoc);
+  };
 
-    useEffect(() => {
-        const getPosts = async () => {
-            const data = await getDocs(postsCollectionRef);
-            setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
-        }
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postsCollectionRef);
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
 
-        getPosts();
-    }, [deletePost])
+    getPosts();
+  }, [deletePost]);
 
-    return <div className='homePage'>
-        {postLists.map((post) => {
-            return <div className="post">
-                <div className='postHeader'>
-                    <div className='title'>
-                        <h1>{post.title}</h1>
-                    </div>
-                    <div className='deletePost'>
-                        {isAuth && post.author.id === auth.currentUser.uid && (<button onClick={() => { deletePost(post.id) }}>&#128465;</button>)}
-                    </div>
-                </div>
-                <div className='postTextContiner'>{post.postText}</div>
-                <h3>@{post.author.name}</h3>
-            </div>;
-        })}
+  return (
+    <div className="homePage">
+      {postLists.map((post) => {
+        return (
+          <div className="post">
+            <div className="postHeader">
+              <div className="title">
+                <h1>{post.title}</h1>
+              </div>
+              <div className="deletePost">
+                {isAuth && post.author.id === auth.currentUser.uid && (
+                  <button
+                    onClick={() => {
+                      deletePost(post.id);
+                    }}
+                  >
+                    &#128465;
+                  </button>
+                )}
+              </div>
+            </div>
+            <div className="postTextContiner">{post.postText}</div>
+            <h3>@{post.author.name}</h3>
+          </div>
+        );
+      })}
     </div>
+  );
 }
 
 export default Home;
